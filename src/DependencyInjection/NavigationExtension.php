e07507dcae94e3f73f47e860e24523cc05ac9748
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-17 13:26:14 -0500

    Delegate canonical extension to runtime-aware loader

--- src/DependencyInjection/NavigationExtension.php
+++ src/DependencyInjection/NavigationExtension.php
@@ -4,63 +4,6 @@ declare(strict_types=1);
 
 namespace App\Navigating\DependencyInjection;
 
-use App\Navigating\Service\Navigation\Validate\NavigationConfigValidateService;
-use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Yaml\Yaml;
-
-final class NavigationExtension extends Extension
+final class NavigationExtension extends NavigationRuntimeExtension
 {
-    /**
-     * @param array<int, array<string, mixed>> $configs
-     */
-    public function load(array $configs, ContainerBuilder $container): void
-    {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, array_merge($this->defaultNavigationConfigs(), $configs));
-        $validationResult = (new NavigationConfigValidateService())->validate($config);
-
-        if (!$validationResult->isValid()) {
-            throw new InvalidConfigurationException(implode(PHP_EOL, $validationResult->errors));
-        }
-
-        $container->setParameter('navigation.config', $config);
-
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
-        $loader->load('services.yaml');
-    }
-
-    /**
-     * @return list<array<string, mixed>>
-     */
-    private function defaultNavigationConfigs(): array
-    {
-        $path = __DIR__.'/../../config/navigation.yaml';
-
-        if (!is_file($path)) {
-            return [];
-        }
-
-        $data = Yaml::parseFile($path);
-
-        if (!is_array($data)) {
-            return [];
-        }
-
-        $config = $data['navigation'] ?? $data;
-
-        if (!is_array($config)) {
-            return [];
-        }
-
-        return [$config];
-    }
-
-    public function getAlias(): string
-    {
-        return 'navigation';
-    }
 }
