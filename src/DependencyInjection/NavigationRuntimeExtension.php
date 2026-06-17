e19f06afa3e96046f1d0209627d6a3a5f782e877
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-17 13:25:35 -0500

    Allow canonical navigation extension wrapper

--- src/DependencyInjection/NavigationRuntimeExtension.php
+++ src/DependencyInjection/NavigationRuntimeExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Yaml\Yaml;
 
-final class NavigationRuntimeExtension extends Extension
+class NavigationRuntimeExtension extends Extension
 {
     /** @param array<int, array<string, mixed>> $configs */
     public function load(array $configs, ContainerBuilder $container): void
