42ea624bd18b0a54bed1ea9d87b85386296745b8
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-18 01:27:00 -0500

    Verify merged namespace metadata

--- tests/NavigationRuntimeActivationConfigTest.php
+++ tests/NavigationRuntimeActivationConfigTest.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class NavigationRuntimeActivationConfigTest extends TestCase
 {
-    public function testDefaultRuntimeActivationIsStrict(): void
+    public function testDefaultRuntimeActivationUsesNamespaceOwnership(): void
     {
         $container = new ContainerBuilder();
         (new NavigationExtension())->load([], $container);
@@ -19,5 +19,13 @@ final class NavigationRuntimeActivationConfigTest extends TestCase
 
         self::assertIsArray($config);
         self::assertTrue($config['runtime_activation']['strict']);
+        self::assertSame(
+            'App\\Interfacing',
+            $config['shell_groups']['left_top']['items']['dashboard']['metadata']['namespace_provider'],
+        );
+        self::assertSame(
+            'App\\Vendoring',
+            $config['shell_groups']['left_middle_business']['items']['vendor']['metadata']['namespace_provider'],
+        );
     }
 }
