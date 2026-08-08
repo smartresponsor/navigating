3a0651670c09a739d5dc448bc7b30bb09235a27b
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-08-08 14:07:40 -0500

    Use crash-safe writer for navigation manifest

--- src/Command/NavigationManifestWriteCommand.php
+++ src/Command/NavigationManifestWriteCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Navigating\Command;
 
+use App\Navigating\Service\Navigation\Snapshot\NavigationSnapshotFileService;
 use App\Navigating\Service\Navigation\Snapshot\NavigationSnapshotService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -13,26 +14,19 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'navigation:manifest:write', description: 'Write the current navigation database as the repository install manifest.')]
 final class NavigationManifestWriteCommand extends Command
 {
-    public function __construct(private readonly NavigationSnapshotService $snapshotService)
-    {
+    public function __construct(
+        private readonly NavigationSnapshotService $snapshotService,
+        private readonly NavigationSnapshotFileService $snapshotFileService,
+    ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $path = dirname(__DIR__, 2).'/resources/navigation/navigation.install.json';
-        $directory = dirname($path);
-
-        if (!is_dir($directory) && !mkdir($directory, 0775, true) && !is_dir($directory)) {
-            $output->writeln('<error>Unable to create navigation manifest directory.</error>');
-            return Command::FAILURE;
-        }
 
         try {
-            $json = json_encode($this->snapshotService->create(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR);
-            if (false === file_put_contents($path, $json.PHP_EOL, LOCK_EX)) {
-                throw new \RuntimeException('Unable to write navigation install manifest.');
-            }
+            $this->snapshotFileService->write($path, $this->snapshotService->create());
         } catch (\Throwable $exception) {
             $output->writeln('<error>'.$exception->getMessage().'</error>');
             return Command::FAILURE;
